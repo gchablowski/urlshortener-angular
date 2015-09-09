8e2fb7334ebe38d492c27755f991f6277ac88be7
gchablowski <g.chablowski@free.fr>
2015-09-09 14:21:23 +0200

    installation of angular with yeoman

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name urlshortenerAngularApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the urlshortenerAngularApp
+ */
+angular.module('urlshortenerAngularApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name urlshortenerAngularApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the urlshortenerAngularApp
+ */
+angular.module('urlshortenerAngularApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
